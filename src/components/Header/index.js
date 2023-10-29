b5db646aada0845f75f5819f5c45f97729f3f35c
Liliam Kaori Yamada <125396573+LiliKaori@users.noreply.github.com>
2023-10-29 20:18:32 -0300

    Atualização estilo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Container } from "@/styles/style"
 import {Li, Menu, Img} from "@/components/Header/style"
-import logoImage from "@/assets/marca-horizontal-branca-e-verde-1024x397.png"
+import logoImage from "@/assets/world-tour-icon.svg"
 
 export default function Header(){
     
@@ -9,7 +9,7 @@ export default function Header(){
         
         <>        
         <Container className="flex-row" $minH="none">
-            <Img src={logoImage.src} alt="logo-imagem" $maxWidth="200px"/>
+            <Img src={logoImage.src} alt="logo-imagem" $maxWidth="130px"/>
             <Menu>
                 <Li onClick={()=>alert("Clicou em Home")}>Home</Li>
                 <Li onClick={() => alert("Clicou em Sobre")}>Sobre</Li>
